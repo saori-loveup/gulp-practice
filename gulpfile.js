3f165e6db28998967c3ef416b18e13cf49e13249
Saori Ito <saori@itoo.info>
2024-09-05 16:07:25 +0900

    watchでstyle.scssの監視して自動コンパイルするように変更

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,24 @@ const gulp = require('gulp');
 // Sassをコンパイルするプラグインの読み込み
 const sass = require('gulp-sass')(require('sass'));
 
-// style.scssのタスクを作成する
+// style.scssの監視タスクを作成する
 gulp.task('default', () => {
-  // style.scssファイルを取得
-  return (
-    gulp
-      .src("css/style.scss")
-      // Sassのコンパイルを実行
-      .pipe(sass())
-      // cssフォルダー以下に保存
-      .pipe(gulp.dest("css"))
-  );
+  // style.scssファイルを監視
+  return gulp.watch("css/style.scss", () => { // style.scssに更新があった場合の処理
+    // style.scssファイルを取得
+    return (
+      gulp
+        .src("css/style.scss")
+        // Sassのコンパイルを実行
+        .pipe(
+          sass({
+            outputStyle: "expanded"
+          })
+          // Sassのコンパイルエラーを表示
+          .on("error", sass.logError)
+        )
+        // cssフォルダー以下に保存
+        .pipe(gulp.dest("css"))
+    );
+  });
 });
