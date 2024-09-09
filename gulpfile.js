cc001f6185c95314244b8c4ba5cfb1e351e7b527
Saori Ito <saori@itoo.info>
2024-09-09 16:03:02 +0900

    Gulp 4.xで推奨されるタスクの定義方式に変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
 // gulpプラグインの読み込み
-const gulp = require('gulp');
+const { src, dest, watch } = require('gulp');
 // Sassをコンパイルするプラグインの読み込み
 const sass = require('gulp-sass')(require('sass'));
 
-// style.scssの監視タスクを作成する
-gulp.task('default', () => {
-  // style.scssファイルを監視
-  return gulp.watch("css/style.scss", () => { // style.scssに更新があった場合の処理
-    // style.scssファイルを取得
-    return (
-      gulp
-        .src("css/style.scss")
-        // Sassのコンパイルを実行
-        .pipe(
-          sass({
-            outputStyle: "expanded"
-          })
-          // Sassのコンパイルエラーを表示
-          .on("error", sass.logError)
-        )
-        // cssフォルダー以下に保存
-        .pipe(gulp.dest("css"))
-    );
-  });
-});
+/**
+ * Sassをコンパイルするタスクの定義
+ */
+const compileSass = () =>
+  // style.scssファイルを取得
+  src("css/style.scss")
+    // Sassのコンパイルを実行
+    .pipe(
+      // コンパイル後のCSSを展開
+      sass({
+        outputStyle: "expanded"
+      })
+    )
+    // cssフォルダー以下に保存
+    .pipe(dest("css"));
+
+/**
+ * Sassファイルを監視し、変更があったらSassを変換します
+ */
+const watchSassFiles = () => watch("css/style.scss", compileSass);
+
+// npx gulpというコマンドを実行した時(タスク名がdefault)、watchSassFilesが実行されるようにする
+exports.default = watchSassFiles;
